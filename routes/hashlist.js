84ab8efdfc94b22dcb950c78149a2e784ea33e55
Mike <mike.orozco94@gmail.com>
2014-07-13 18:48:22 -0700

    Removed comments and unnecessary logging
    Removed unnecessary comments and logging from hashlist.js.

--- routes/hashlist.js
+++ routes/hashlist.js
@@ -2,17 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 function isEmpty(obj) {
-	// null and undefined are "empty"
 	if (obj == null) return true;
-	 
-	// Assume if it has a length property with a non-zero value
-	// that that property is correct.
+
 	if (obj.length && obj.length > 0) return false;
 	if (obj.length === 0) return true;
-	 
-	// Otherwise, does it have any properties of its own?
-	// Note that this doesn't handle
-	// toString and toValue enumeration bugs in IE < 9
+	
 	for (var key in obj) {
 		if (hasOwnProperty.call(obj, key)) return false;
 	}
@@ -37,10 +31,6 @@ router.get('/', function(req, res) {
     var output = "";
 
     var download = false;
-
-    console.log(query);
-
-
 
     if(query.dl == '' || !isEmpty(query.dl)) {
     	download = true;
